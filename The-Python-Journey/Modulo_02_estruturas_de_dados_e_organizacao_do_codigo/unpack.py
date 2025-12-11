21ef28dd540b5a66e499d0c8cee8b8e2d9e8d85e
Stwarty <stwartyjcamara@hotmail.com>
2025-12-11 13:23:48 -0300

    Enhance unpacking examples for lists and tuples with additional explanations and outputs

--- The-Python-Journey/Modulo_02_estruturas_de_dados_e_organizacao_do_codigo/unpack.py
+++ The-Python-Journey/Modulo_02_estruturas_de_dados_e_organizacao_do_codigo/unpack.py
@@ -1,7 +1,21 @@
 '''
 Empacotamento e desempacotamento de listas e tuplas
 '''
+# Empacotamento [Listas]
+nomes = ["Ana", "Bruno", "Carla"]
+print(nomes,'\n')
+# Saída: ['Ana', 'Bruno', 'Carla']
 nome1, *resto_nomes = ["Ana", "Bruno", "Carla", "Daniel"]
-print(nome1, resto_nomes)
+print(nome1, resto_nomes,'\n') # o * antes do resto_nomes indica que é uma lista
 # Saída: Ana ['Bruno', 'Carla', 'Daniel']
 nome2, nome3, *resto_nomes = ("Ana", "Bruno", "Carla", "Daniel", "Eduardo")
+print(f'{nome2}, {nome3}, {resto_nomes}\n') #fstring
+# Saída: Ana Bruno ['Carla', 'Daniel', 'Eduardo']
+nome1, nome2, *_ = ["Ana", "Bruno", "Carla", "Daniel", "Eduardo"]
+print(nome1, nome2, *_,'\n') # o _ sgnifica que o valor não será usado
+# Saída: Ana Bruno Carla Daniel Eduardo
+'''
+tuplas = listas imutáveis
+'''
+nome = 'Ana', 'Bruno', 'Carla'  # tupla implícita
+print(nome)
